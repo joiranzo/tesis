33ca1aabc12eafd5ef5598a4aca0a5853e2cba0b
josé Iranzo <joiranzo@gmail.com>
2024-06-07 11:05:43 -0300

    Cambiios

---
+++ AW/tienda.js
@@ -0,0 +1,10 @@
+let m2hombre=document.getElementById("menu2_hombre");
+let m2=document.getElementById("menu2");
+let m2h=document.getElementById("menu2_sub_hombre");
+
+m2hombre.addEventListener("click",(e)=>{
+    m2.style.backgroundSize="100% 80%"
+    
+    m2h.style.visibility="visible"
+    
+});
