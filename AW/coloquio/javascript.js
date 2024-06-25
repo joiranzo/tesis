f5b83c1434634cc3a368d47fb4eb0f0814dbb257
joiranzo <joiranzo@gmail.com>
2024-06-25 12:13:06 -0300

    Final

--- AW/coloquio/javascript.js
+++ AW/coloquio/javascript.js
@@ -39,6 +39,36 @@ function mostrar(){
     ventana.classList.toggle("visible")
 }
 
+function izquierda(){
+    let elementos=document.getElementsByClassName("movible")
+    let precio=elementos[0].children[1].innerHTML
+    let fuente=elementos[0].children[0].src
+    
+    elementos[0].children[0].src=elementos[1].children[0].src;
+    elementos[0].children[1].innerHTML=elementos[1].children[1].innerHTML;
+    
+    elementos[1].children[0].src=elementos[2].children[0].src;
+    elementos[1].children[1].innerHTML=elementos[2].children[1].innerHTML;
+
+    elementos[2].children[0].src=fuente;
+    elementos[2].children[1].innerHTML=precio;
+}
+
+function derecha(){
+    let elementos=document.getElementsByClassName("movible")
+    let precio=elementos[2].children[1].innerHTML
+    let fuente=elementos[2].children[0].src
+    
+    elementos[2].children[0].src=elementos[1].children[0].src;
+    elementos[2].children[1].innerHTML=elementos[1].children[1].innerHTML;
+    
+    elementos[1].children[0].src=elementos[0].children[0].src;
+    elementos[1].children[1].innerHTML=elementos[0].children[1].innerHTML;
+
+    elementos[0].children[0].src=fuente;
+    elementos[0].children[1].innerHTML=precio;
+}
+
 function carga(){
     mostrar_fecha();
     mueveReloj()
