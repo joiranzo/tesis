f9e929cd3a93eaf3281295025c6261b7c5473d33
joiranzo <joiranzo@gmail.com>
2024-07-26 13:35:55 -0300

    hola

--- Variables/php/motivos.php
+++ Variables/php/motivos.php
@@ -1,60 +1,67 @@
-<a?php 
+<?php
 
-include("conexion.php");
+include ("conexion.php");
 
-$con=conectar();
+$con = conectar();
 
 
-$sql="SELECT * FROM Motivos";
+$sql = "SELECT * FROM Motivos";
 
-$query=mysqli_query($con,$sql);
+$query = mysqli_query($con, $sql);
 
-$row=mysqli_fetch_array($query);
+$row = mysqli_fetch_array($query);
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/Variables/css/TablasAux.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
     <title>Motivos</title>
 </head>
+
 <body>
-  
-        <form action="insert.php" >
+    <div class="contenedor">
+        <!--<form action="insert.php" >
             <h3>Nuevo Motivo</h3>
             <input type="text" class="" name="CodMotivo" id="" placeholder="Código del Motivo">
             <input type="text" class="" name="NomMotivo" id="" placeholder="Motivo">
             <button type="submit">Enviar</button>
-        </form>
-  
-    
-    <table>
-        <thead>
-            <tr>
-                <th>Código</th>
-                <th>Descripción</th>
-                <th></th>
-                <th></th>
-            </tr>
-        </thead>
-        <tbody>
-        <?php
-                while($row=mysqli_fetch_array($query)){
-            ?>
-                <tr>
-                    <td class="center"><?php echo $row['CodMotivo']?></td>
-                    <td><?php echo $row['NomMotivo']?></td>
-                    <!--<td class="center"><a>Editar</a></td>
-                    <td class="center"><a>Borrar</a></td>-->
-                </tr>
-            <?php
+        </form>-->
+        <div class="barra">
+            <h3>Motivos</h3>
+            <div class="menu c4">
+                <a class="material-symbols-outlined" href="#" >add_box</a>
+                <a class="material-symbols-outlined" href="http://" target="_blank" rel="noopener noreferrer">home</a>
+            </div>
+        </div>
+        <div class="table">
+            <div class="cabecera">
+                <h3 class="c1">Código</h3>
+                <h3 class="c2">Descripción</h3>
+                <h3 class="c3"></h3>
+                <h3 class="c4"></h3>
+            </div>
+            <div class="cuerpo">
+                <?php
+                while ($row = mysqli_fetch_array($query)) {
+                    ?>
+                    <div class="fila">
+                        <h5 class="c1"><?php echo $row['CodMotivo'] ?></>
+                            <h5 class="c2"><?php echo $row['NomMotivo'] ?></h5>
+                            <a class="c3 material-symbols-outlined" href="http://" target="_blank" rel="noopener noreferrer">Edit</a>
+                            <a class="c4 rojo material-symbols-outlined" href="http://" target="_blank" rel="noopener noreferrer">delete</a>
+                    </div>
+                    <?php
                 }
-            ?> 
-        </tbody>
-    </table>
-    
+                ?>
+            </div>
+        </div>
+    </div>
 </body>
+
 </html>
