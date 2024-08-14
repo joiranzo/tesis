f8bf7a1815036571462dec05b8bd47115d13be2e
joiranzo <joiranzo@gmail.com>
2024-08-13 21:14:49 -0300

    Final martes

--- Variables/public/js/empleado.js
+++ Variables/public/js/empleado.js
@@ -1,7 +1,5 @@
 addEventListener("DOMContentLoaded",(e)=>{
-    
    
-    
     const buscarDato=async (datos) =>{
         
         try {
@@ -13,29 +11,22 @@ addEventListener("DOMContentLoaded",(e)=>{
 
             return req.json()
         } catch (error){alert(error.message)}
-
     }
 
     const getEmpleado=async  (legajo)=>{
         buscarDato({tabla:"Dotacion",key:"Legajo",filtro:legajo})
         .then (data=>{
-            
             document.getElementById("Legajo").setAttribute("value",data[0].Legajo)
             document.getElementById("NomApe").setAttribute("value",data[0].NomApe)
             document.getElementById("Email").setAttribute("value",data[0].Email)
             document.getElementById("RelPuesto").value=data[0].RelPuesto
             document.getElementById("RelReprGremial").value=data[0].RelReprGremial
-            document.getElementById("SupInmed").value=data[0].RelSupInmed
-
-            // document.getElementById("RelPuesto")=data[0].RelPuesto
-            // document.getElementById("RelPuesto")=data[0].RelPuesto
-            // document.getElementById("RelPuesto")=data[0].RelPuesto
-            // document.getElementById("RelPuesto")=data[0].RelPuesto
-            // document.getElementById("RelPuesto")=data[0].RelPuesto
-            // document.getElementById("RelPuesto")=data[0].RelPuesto
-            // document.getElementById("RelPuesto")=data[0].RelPuesto
-
-
+            document.getElementById("RelSupInmed").value=data[0].RelSupInmed
+            document.getElementById("RelSupervision").value=data[0].RelSupervision
+            document.getElementById("RelSubregion").value=data[0].RelSubRegion
+            document.getElementById("RelEncargTiempo").value=data[0].RelEncargTiempos
+            document.getElementById("RelCoordinador").value=data[0].RelCoordinador
+            document.getElementById("RelCoordinacion").value=data[0].RelCoordinacion
             document.getElementById("buscar").style.display="none"
             document.getElementById("editar").style.display="flex"
         })
@@ -58,7 +49,7 @@ addEventListener("DOMContentLoaded",(e)=>{
     // const inps=document.getElementsByClassName("editable")
     // const nuevo=document.getElementById("fila_nueva")
     // const btn_cancel_nuevo=document.getElementById("cancel_new")
-    const forms=document.getElementsByClassName("fila")
+    const forms=document.getElementsByClassName("fila_hover")
     var prueba
 
     for (const key in btns_edit) {
@@ -67,14 +58,8 @@ addEventListener("DOMContentLoaded",(e)=>{
         
         btn.addEventListener("click",(e)=>{
             e.preventDefaul
-            prueba=getEmpleado(forms[key].childNodes[1].innerText)
-            //let puestos=getDatos({tabla:"Puestos",Key:null})
-           
-            console.log(prueba)
+            getEmpleado(forms[key].childNodes[1].innerText)
         
-            
-            
-            
         })
     }}};
 })    
