9fcdad34dc379de929b76c7cc7977593f629e228
joiranzo <joiranzo@gmail.com>
2024-08-20 20:32:52 -0300

    final martes

--- Variables/public/js/novedadesxgremio.js
+++ Variables/public/js/novedadesxgremio.js
@@ -3,26 +3,9 @@ addEventListener("DOMContentLoaded",(e)=>{
     const corresponde=document.getElementsByName("corresponde")
     const activar=document.getElementById("btn_activar")
     const desactivar=document.getElementById("btn_desactivar")
-    const si=document.getElementById("si")
-
-    function insertar (nuevo,elemento) {
-        if (elemento.children.length==0) {
-            elemento.append(nuevo)
-        } else {
-
-        for (const element of elemento.children) {
-            if (nuevo.children[0].innerHTML>element.children[0].innerHTML) {}
-            else { 
-                elemento.insertBefore(nuevo,element)
-                
-            }
-        }}
-         
-        
-            
-        
-    }
-
+    // const si=document.getElementById("si")
+    const no=document.getElementById("no")
+    
     for (const key in nocorresponde) {
         if (Object.prototype.hasOwnProperty.call(nocorresponde, key)) {
             const element = nocorresponde[key];
@@ -46,30 +29,21 @@ addEventListener("DOMContentLoaded",(e)=>{
     }
 
     activar.addEventListener("click",e=>{
-        for (const key in nocorresponde) {
-            if (Object.prototype.hasOwnProperty.call(nocorresponde, key)) {
-                const element = nocorresponde[key];
-                
-                if (element.classList.contains("seleccionado")){
-                    element.classList.toggle("seleccionado")
-                    element.parentElement.removeChild(element)
-                    insertar(element,si)
-                }
+        for (const element of no.children) {
+            if (element.classList.contains("seleccionado")){
+                element.classList.toggle("seleccionado")
+                element.parentElement.removeChild(element)
+                si.append(element)
             }
         }
     })
 
     desactivar.addEventListener("click",e=>{
-        for (const key in corresponde) {
-            if (Object.prototype.hasOwnProperty.call(corresponde, key)) {
-                const element = corresponde[key];
-                
-                if (element.classList.contains("seleccionado")){
-                    element.classList.toggle("seleccionado")
-                    element.style.display="none"
-                    nocorresponde[key].style.display="block"
-
-                }
+        for (const element of si.children) {
+            if (element.classList.contains("seleccionado")){
+                element.classList.toggle("seleccionado")
+                element.parentElement.removeChild(element)
+                no.append(element)
             }
         }
     })
