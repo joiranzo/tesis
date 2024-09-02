5ea5058a39f9350831bc5ea9b4ccd2e5cc048115
joiranzo <joiranzo@gmail.com>
2024-09-02 19:16:18 -0300

    Ok Clase 2 ED4

---
+++ Issd/Semestre4/Ed4/Lista.java
@@ -0,0 +1,25 @@
+package Issd.Semestre4.Ed4;
+
+public class Lista {
+
+
+    public static void main(String[] args) {
+        System.out.println("Prueba de pilas");
+        Pila pila1= new Pila();
+        pila1.insertar(123);
+        pila1.insertar(456);
+        pila1.insertar(789);
+        pila1.imprimir();
+        System.out.println("Extraemos uno de la pila:"+pila1.extraer());
+        pila1.imprimir();
+    
+        System.out.println("Prueba de colas");
+        Cola cola1=new Cola();
+        cola1.insertar(5);
+        cola1.insertar(10);
+        cola1.insertar(50);
+        cola1.imprimir();
+        System.out.println("Extraemos uno de la cola:"+cola1.extraer());
+        cola1.imprimir();
+    }
+}
---
+++ Issd/Semestre4/Ed4/Pila.java
@@ -0,0 +1,57 @@
+package Issd.Semestre4.Ed4;
+
+public class Pila{
+    
+    class Nodo{
+        int info;
+        Nodo sig;
+    }
+
+    private Nodo raiz; 
+    
+    Pila(){
+        raiz=null;
+    }
+
+    public boolean vacia(){
+        if (raiz==null)
+            return true;
+        else    
+            return false;    
+    }
+
+    public void insertar(int x){
+        Nodo nuevo= new Nodo();
+        nuevo.info=x;
+        if (raiz==null){
+            raiz=nuevo;
+            nuevo.sig=null;
+        } 
+        else{
+            nuevo.sig=raiz;
+            raiz=nuevo;
+        }
+    }
+
+    public void imprimir(){
+        Nodo reco =raiz;
+        System.out.println("Listado de todos los elementos de la Lista");
+        while (reco !=null){
+            System.out.print(reco.info + "-");
+            reco=reco.sig;
+        }
+        System.out.println();
+    }
+
+    public int extraer(){
+        if (!vacia()){
+            int informacion=raiz.info;
+            raiz=raiz.sig;
+            return informacion;
+        }   
+        else
+            return Integer.MAX_VALUE;
+    }
+  
+}
+
