3dbeeba674b0caeb192f3ebb6278beff59c68889
joiranzo <joiranzo@gmail.com>
2024-12-10 12:46:48 -0300

    Delete tesis2 directory

--- tesis2/back/src/models/tipoRol.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-    const tipoRol = mongoose.Schema(({
-        _id:{type:String, require:true , trim:true, index:true}
-        
-    }));
-
-export default tipoRol
